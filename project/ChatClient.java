41c4df31c37757fe46fb6e3ddfbfff7c7b246949
er02990 <54376608+er02990@users.noreply.github.com>
2020-11-19 18:24:53 -0500

    Create ChatClient.java

---
+++ project/ChatClient.java
@@ -0,0 +1,154 @@
+package net.codejava.networking.chat.client;
+ 
+import java.net.*;
+import java.io.*;
+ 
+/**
+ * This is the chat client program.
+ * Type 'bye' to terminte the program.
+ *
+ * @author www.codejava.net
+ */
+public class ChatClient {
+    private String hostname;
+    private int port;
+    private String userName;
+ 
+    public ChatClient(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+ 
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+ 
+            System.out.println("Connected to the chat server");
+ 
+            new ReadThread(socket, this).start();
+            new WriteThread(socket, this).start();
+ 
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+ 
+    }
+ 
+    void setUserName(String userName) {
+        this.userName = userName;
+    }
+ 
+    String getUserName() {
+        return this.userName;
+    }
+ 
+ 
+    public static void main(String[] args) {
+        if (args.length < 2) return;
+ 
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+ 
+        ChatClient client = new ChatClient(hostname, port);
+        client.execute();
+    }
+}
+The ReadThread is responsible for reading input from the server and printing it to the console repeatedly, until the client disconnects. This class is implemented as follows:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+package net.codejava.networking.chat.client;
+ 
+import java.io.*;
+import java.net.*;
+ 
+/**
+ * This thread is responsible for reading server's input and printing it
+ * to the console.
+ * It runs in an infinite loop until the client disconnects from the server.
+ *
+ * @author www.codejava.net
+ */
+public class ReadThread extends Thread {
+    private BufferedReader reader;
+    private Socket socket;
+    private ChatClient client;
+ 
+    public ReadThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+ 
+        try {
+            InputStream input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+ 
+    public void run() {
+        while (true) {
+            try {
+                String response = reader.readLine();
+                System.out.println("\n" + response);
+ 
+                // prints the username after displaying the server's message
+                if (client.getUserName() != null) {
+                    System.out.print("[" + client.getUserName() + "]: ");
+                }
+            } catch (IOException ex) {
+                System.out.println("Error reading from server: " + ex.getMessage());
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+}
